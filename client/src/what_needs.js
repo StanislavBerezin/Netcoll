7a301e01e12c9496c91a71b6aa9d0905bf48af45
StanislavBerezin <stanislav_berezin@hotmail.com>
2018-01-09 19:29:40 +1000

    test again myBranch

--- client/src/what_needs.js
+++ client/src/what_needs.js
@@ -1,6 +1,6 @@
 /*
     //NOT urgent
-    //INTRO PAGE again from my branch
+    //INTRO PAGE again test 2, see what is inside
     1) Mobile respnsivness for the top menu => should have a dropdown for login, register
     2) Need to add section ('About us')
     3) Functionality to compare passwords on UI
